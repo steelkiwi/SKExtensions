29ae8a4493ad38bb12fdb9d402f74c4163912465
Viktor Olesenko <OlesenkoViktor@gmail.com>
2017-06-05 11:12:03 +0300

    Updated TableView cell register

--- UITableView+CellRegister.swift
+++ UITableView+CellRegister.swift
@@ -1,6 +1,6 @@
 //
 //  UITableView+CellRegister.swift
-//  RDXT
+//
 //
 //  Created by Viktor Olesenko on 04.05.17.
 //  Copyright © 2017 Steelkiwi. All rights reserved.
@@ -16,27 +16,31 @@ extension UITableView {
     
     /// Register cell wrapper function
     /// Register passed cell class with class name as identifier
-    /// 
-    /// tableView.register(SubclassCell.self, forCellReuseIdentifier: identifier) -> tableView.register.register(SubclassCell.self, isNib: false)
     ///
-    /// - Parameter cellClass:  UITableViewCell subclass class
-    /// - Parameter isNib:      Is cell stored in .xib file or just in code. Default is true
-    func register<T>(_ cellClass: T.Type, isNib: Bool = true) where T: UITableViewCell {
+    /// tableView.register(SubclassCell.self, forCellReuseIdentifier: identifier) -> tableView.register(SubclassCell.self, isNib: false, forCellReuseIdentifier: identifier)
+    ///
+    /// - Parameters:
+    ///   - cellClass:  UITableViewCell subclass class
+    ///   - isNib:      Is cell stored in .xib file or just in code. Default is true
+    ///   - identifier: The reuse identifier to associate with the specified nib file. Default value is `identifier` property.
+    func register<T>(_ cellClass: T.Type, isNib: Bool = true, forCellReuseIdentifier identifier: String = T.identifier) where T: UITableViewCell {
         if isNib {
-            self.register(UINib(nibName: String(describing: T.classForCoder()), bundle: nil), forCellReuseIdentifier: T.identifier)
+            self.register(UINib(nibName: String(describing: T.classForCoder()), bundle: nil), forCellReuseIdentifier: identifier)
         } else {
-            self.register(T.classForCoder(), forCellReuseIdentifier: T.identifier)
+            self.register(T.classForCoder(), forCellReuseIdentifier: identifier)
         }
     }
     
     
     /// Cell deque wrapper
     ///
-    /// let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as! SubclassCell -> let cell: SubclassCell = tv.dequeueReusableCell()
+    /// let cell = tableView.dequeueReusableCell(withIdentifier: identifier) as! SubclassCell -> let cell: SubclassCell = tv.dequeueReusableCell(withIdentifier: identifier)
     ///
+    /// - Parameters:
+    ///   - identifier: The reuse identifier to associate with the specified nib file. Default value is `identifier` property.
     /// - Returns: Dequed cell for passed as generic class. Can fail if cell not registered
-    func dequeueReusableCell<T>() -> T where T: UITableViewCell {
-        guard let cell = self.dequeueReusableCell(withIdentifier: T.identifier) as? T else {
+    func dequeueReusableCell<T>(withIdentifier identifier: String = T.identifier) -> T where T: UITableViewCell {
+        guard let cell = self.dequeueReusableCell(withIdentifier: identifier) as? T else {
             assertionFailure("Cell \(T.classForCoder()) not registered in tableView")
             return T()
         }
