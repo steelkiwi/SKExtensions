2e5495157283c719ce4c2f015e320a06327fad25
Viktor Olesenko <OlesenkoViktor@gmail.com>
2018-06-05 10:20:56 +0300

    General - File structure upd

---
+++ Sources/Array+Filter.swift
@@ -0,0 +1,22 @@
+//
+//  Array+Filter.swift
+//  
+//
+//  Created by Viktor Olesenko on 27.04.18.
+//  Copyright © 2018 Steelkiwi Inc. All rights reserved.
+//
+
+import Foundation
+
+public extension Array {
+    
+    // MARK: - Filter
+    
+    /// Filter array and returns list of unique passed objects / properties (should conform to Hashable protocol). Example: `unique({ $0.type })` returns an Array of `type` class objects
+    func uniqueElements<ElementOfResult: Hashable>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> Array<ElementOfResult> {
+        let elements = try self.compactMap(transform)
+        let uniqueSet = Set<ElementOfResult>.init(elements)
+        
+        return Array<ElementOfResult>.init(uniqueSet)
+    }
+}
--- Sources/Array+Shuffle.swift
+++ Sources/Array+Shuffle.swift
@@ -1,29 +1,19 @@
 //
-//  Array.swift
+//  Array+Shuffle.swift
 //  
 //
-//  Created by Viktor Olesenko on 27.04.18.
+//  Created by Viktor Olesenko on 05.06.18.
 //  Copyright © 2018 Steelkiwi Inc. All rights reserved.
 //
 
 import Foundation
 
-extension Array {
-    
-    // MARK: - Filter
-    
-    /// Filter array and returns list of unique passed objects / properties (should conform to Hashable protocol). Example: `unique({ $0.type })` returns an Array of `type` class objects
-    func unique<ElementOfResult: Hashable>(_ transform: (Element) throws -> ElementOfResult?) rethrows -> Array<ElementOfResult> {
-        let elements = try self.compactMap(transform)
-        let uniqueSet = Set<ElementOfResult>.init(elements)
-        
-        return Array<ElementOfResult>.init(uniqueSet)
-    }
+public extension Array {
     
     // MARK: - Shuffle
     // https://github.com/mergesort/Public-Extension/blob/54a76d67f9ea8de4605ecb57cc42e953748d6620/PublicExtension.playground/Pages/Array.xcplaygroundpage/Contents.swift#L54-L71
     
-    mutating func shuffle() {
+    public mutating func shuffle() {
         // Empty and single-element collections don't shuffle
         guard count > 1 else { return }
         
