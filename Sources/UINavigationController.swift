0147ea9b6899fff36bef446a62ebb569a033faa5
Viktor Olesenko <OlesenkoViktor@gmail.com>
2018-06-15 11:05:15 +0300

    UINavigationController - added replace

---
+++ Sources/UINavigationController.swift
@@ -0,0 +1,22 @@
+//
+//  UINavigationController.swift
+//  
+//
+//  Created by Viktor Olesenko on 15.06.18.
+//  Copyright © 2018 Steelkiwi. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    
+    func replace(vc oldVC: UIViewController, with newVC: UIViewController, duration: TimeInterval = 0.35, animationOptions: UIViewAnimationOptions?, completion: ((Bool) -> Void)? = nil) {
+        guard let index = self.viewControllers.index(of: oldVC) else { return }
+        
+        self.viewControllers[index] = newVC
+        
+        if let animationOptions = animationOptions {
+            UIView.transition(from: oldVC.view, to: newVC.view, duration: duration, options: animationOptions, completion: completion)
+        }
+    }
+}
