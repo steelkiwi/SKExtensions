a9c7fd69953c0ce5043aef1805def3e9d9f76466
Viktor Olesenko <OlesenkoViktor@gmail.com>
2018-12-04 08:16:08 +0200

    Array - contains changes

--- Sources/Array+Subarray.swift
+++ Sources/Array+Subarray.swift
@@ -10,7 +10,20 @@ import UIKit
 
 public extension Array where Element: Equatable {
     
+    /// Returns a Boolean value indicating whether the sequence contains an elements from the given subArray
     public func contains(_ subArray: [Element]) -> Bool {
+        for element in subArray {
+            guard self.contains(element) else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    /// Returns true, if current array contains all elements from subarray in same order and without breaks
+    /// Example: [A, B, C] contains [B, C], but not contains [A, C]
+    public func contains(inseparable subArray: [Element]) -> Bool {
         
         // This is to prevent construction of a range from zero to negative
         guard self.count >= subArray.count else { return false }
