53f66533c5c55ff9803db4b515a238671d15c2af
Viktor Olesenko <OlesenkoViktor@gmail.com>
2019-07-30 09:52:40 +0300

    UserDefaults - UIColor

---
+++ SKExtensions/Sources/UserDefaults+UIColor.swift
@@ -0,0 +1,28 @@
+//
+//  UserDefaults+UIColor.swift
+//  Gotribe
+//
+//  Created by Viktor Olesenko on 30.07.19.
+//
+
+import UIKit
+
+extension UserDefaults {
+    
+    func color(forKey key: String) -> UIColor? {
+        guard let data = data(forKey: key) else { return nil }
+        
+        return try? NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? UIColor
+    }
+    
+    func set(_ color: UIColor?, forKey key: String) {
+        var data: Data?
+        
+        if let color = color,
+            let archivedData = try? NSKeyedArchiver.archivedData(withRootObject: color, requiringSecureCoding: false) {
+            data = archivedData
+        }
+        
+        self.set(data, forKey: key)
+    }
+}
